d6fa3110e203871f0d43c4fcdfe242f9be9649e9
Aswin <108606894+Aswgt999@users.noreply.github.com>
2025-10-05 10:46:29 +0000

    Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// function to get the no of squares and to generate board
 function promptAndGenerateSquares() {
     const noOfSquares = parseInt(prompt("How many squares(limit=100)?"));
     if (noOfSquares > 100) {
@@ -29,34 +30,10 @@ function promptAndGenerateSquares() {
         });
     });
     }
-    // const container = document.querySelector(".container");
-    
-    // // Initializing container for etch-a-sketch
-    // container.style.border = "5px solid black";
-    // container.innerHTML = "";
-    // container.style.width = (noOfSquares * 10) + "px";
-    // container.style.height = (noOfSquares * 10) + "px";
-    
-    // // Generating etch-a-sketch board 
-    // for (let i = 0; i < noOfSquares; i++) {
-    //     for (let j = 0; j < noOfSquares; j++) {
-    //         const gridSquare = document.createElement('div');
-    //         gridSquare.className = "square";
-    //         container.appendChild(gridSquare);
-    //     } 
-    // }
-
-    // // Code for persistence of hover effect
-    // document.querySelectorAll('.square').forEach(square => {
-    // square.addEventListener('mouseenter', () => {
-    // square.classList.add('drawn');
-    //     });
-    // });
 }
 
+// Function to clear sketch
 function clearSketch() {
-    // const container = document.querySelector(".container");
-    // container.innerHTML = "";
     document.querySelectorAll('.square').forEach(square => {
         square.style.transition = 'none';
         square.classList.remove('drawn');
@@ -65,5 +42,4 @@ function clearSketch() {
             square.style.transition = '';
         });
     });
-
 }
